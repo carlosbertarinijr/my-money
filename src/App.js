8cdc80d15b7c988e7c3815c6397583df3a752cd7
carlos-lity <bertarinijuniorcarlos@gmail.com>
2020-09-22 15:05:38 -0300

    axios/react-router-dom/conexão com firebase

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import axios from 'axios'
 
+// axios
+// .get('https://mymoney-top.firebaseio.com/valor.json')
+// .then(success => {
+//   console.log(success)
+// })
+
+// axios
+// .post('https://mymoney-top.firebaseio.com/valor.json', {outro: 'carlos'})
+// .then(success => {
+//   console.log(success.data)
+// })
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>myMoney</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
