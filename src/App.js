70b1cabb56fe79d432b86d3bb1edb7c4b18a7be9
carlos-lity <bertarinijuniorcarlos@gmail.com>
2020-09-23 16:11:51 -0300

    hook usePost personalizado

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React from 'react'
 import useGet from './useGet'
+import usePost from './usePost'
+
 const url = 'https://mymoney-top.firebaseio.com/movimentacoes/2020-09.json'
 
 
 function App() {
   const data = useGet(url)
+  const [postData, post] =  usePost(url)
 
+  const saveNew = () => {
+    post({valor: 10, descricao: 'Olá Wayner'})
+  }
   return (
     <div>
       <h1>myMoney</h1>
       {JSON.stringify(data)}
       {data.loading && <p>Carregando...</p>}
+      <button onClick={saveNew}>Salvar</button>
+      <pre>{JSON.stringify(postData)}</pre>
     </div>
   )
 }
