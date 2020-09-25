9f13a43f5fc1808b74ca1f0aeac8b3f950a7b1dc
carlos-lity <bertarinijuniorcarlos@gmail.com>
2020-09-25 16:44:03 -0300

    adicionar mÊs

--- src/pages/Movimentacoes.js
+++ src/pages/Movimentacoes.js
@@ -1,13 +1,39 @@
+import { useState } from 'react'
 import React from 'react'
 
 import Rest from '../utils/rest'
 
 const baseUrl = 'https://mymoney-top.firebaseio.com/'
-const { useGet } = Rest(baseUrl)
+const { useGet, usePost, useDelete } = Rest(baseUrl)
 
 const Movimentacoes = ({match}) => {
     const data = useGet(`movimentacoes/${match.params.data}`)
+    const [postData, salvar] = usePost(`movimentacoes/${match.params.data}`)
+    const [removeData, remover] = useDelete()
+    const [ descricao, setDescricao ] = useState('')
+    const [ valor, setValor ] = useState('')
 
+    const onChangeDescricao = evt => {
+        setDescricao(evt.target.value)
+    }
+    const onChangeValor = evt => {
+        setValor(evt.target.value)
+    }
+    const salvarMovimentacao = async() => {
+        if(!isNaN(valor) && valor.search(/^[-]?\d+(\.)?\d+?$/) >=0 ) {
+            await salvar( {
+                descricao,
+                valor: parseFloat(valor)
+            })
+            setDescricao('')
+            setValor(0)
+            data.refetch()
+        }
+    }
+    const removerMovimentacao = async(id) => {
+        await remover(`movimentacoes/${match.params.data}/${id}`)
+        data.refetch()
+    }
     return(
         <div className='container'>
             <h1>Movimentacoes</h1>
@@ -26,12 +52,25 @@ const Movimentacoes = ({match}) => {
                                 return(
                                     <tr>
                                         <td>{data.data[movimentacao].descricao}</td>
-                                        <td>{data.data[movimentacao].valor}</td>
+                                        <td className='text-right'>
+                                            {data.data[movimentacao].valor} {' '}
+                                            <button onClick={() => removerMovimentacao(movimentacao)} className='btn btn-danger'>-</button>
+                                        </td>
                                     </tr>
                                 )
                             })
                     }
+                    <tr>
+                        <td>
+                            <input type='text' value={descricao} onChange={onChangeDescricao}/>
+                        </td>
+                        <td>
+                            <input type='text' value={valor} onChange={onChangeValor}/>
+                            <button onClick={salvarMovimentacao} className='btn btn-success'>+</button>
+                        </td>
+                    </tr>
                 </tbody>
+
             </table>
         </div>
         
